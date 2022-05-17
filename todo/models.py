1d945979d46f58a2b03276095f3d4c47f503fd10
Andrey96 <mulinov.andrey@gmail.com>
2022-05-17 21:49:01 +0300

    Todo project 1.1

--- todo/models.py
+++ todo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Todo(models.Model):
+    title = models.CharField(max_length=100) #создание модели заголовка
+    memo = models.TextField(blank=True) #создание модели описания, blank=True делает это поле необязательным для заполнения
+    created = models.DateTimeField(auto_now_add=True) #создание модели времени поста, auto_now_add=True автоматически добавляет время поста
+    datecompleted = models.DateTimeField(null=True, blank=True)
+    important = models.BooleanField(default=False) # создание модели важности задачи, defaul=False определяет необязательность проставления важности задачи
+    user = models.ForeignKey(User, on_delete=models.CASCADE) # для определения связи между записью и пользователем, оно осуществляется благодаря уникальному ID у каждого пользователя
+
+    def __str__(self):
+        return self.title # функция необходимая для отображения названия заметки в панели админа
